2775fd2b11516d46deccaf10b0c6a3e1406cb860
Bilal Syed Ali <Bilal.s.ali27@gmail.com>
2024-04-09 15:24:31 +0200

    Trying to push

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,7 +1,27 @@
 import tkinter as tk
-#To make a more easy to use and better understanding calculator
-
 
 root = tk.Tk()
-root.geometry("300x275")
+root.title("simpleCalculator")
+
+#Makes the symbols on the buttons
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    'C', '0', '=', '+'
+]
+
+row = 0
+col = 0
+
+#Makes the buttons and gives them size and font
+for button in buttons:
+    bn = tk.Button(root, text=button, font=("Arial", 18), padx=20, pady=10)
+    bn.grid(row=row, column=col)
+    #First adds columns then stops the column after its reached 3 columns and then starts to add rows
+    col += 1
+    if col > 3:
+        col = 0
+        row += 1
+
 root.mainloop()
